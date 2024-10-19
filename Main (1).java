98cfd6118c24cc2260a0d0df1b3afb0d2b17126c
Citla2804 <mercadercitlali@gmail.com>
2024-10-19 10:23:12 -0600

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,10 @@
+package rpg;
+
+import rpg.entities.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();  // Cambiar start() por startGame()
+    }
+}
